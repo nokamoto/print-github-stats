71bf89d16b7c6a035db77abf83df5472f39d2d8a
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2019-11-28 14:28:34 +0900

    Print approve, deletions, additions, and reviews in stats sub command

--- cmd/stats.go
+++ cmd/stats.go
@@ -1,8 +1,19 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/google/go-github/v28/github"
+	"net/url"
+	"time"
+)
+
+const (
+	startKey = "start-date"
+	endKey = "end-date"
 )
 
 var statsCmd =  &cobra.Command{
@@ -12,6 +23,182 @@ var statsCmd =  &cobra.Command{
 	Run: stats,
 }
 
+func init() {
+	flags := statsCmd.Flags()
+
+	now := time.Now()
+	flags.String(endKey, fmt.Sprintf("%d-%2d-%2d", now.Year(), now.Month(), now.Day()), `retrieves pull requests created before the date`)
+	viper.BindPFlag(endKey, flags.Lookup(endKey))
+
+	now = now.AddDate(0, -1, 0)
+	flags.String(startKey, fmt.Sprintf("%d-%2d-%2d", now.Year(), now.Month(), now.Day()), `retrieves pull requests created after the date`)
+	viper.BindPFlag(startKey, flags.Lookup(startKey))
+}
+
+func listRepos(ctx context.Context, client *github.Client, org string) []*github.Repository {
+	opts := &github.RepositoryListByOrgOptions{ListOptions: github.ListOptions{PerPage: 30}}
+
+	var repos []*github.Repository
+	for {
+		result, res, err := client.Repositories.ListByOrg(ctx, org, opts)
+		if err != nil {
+			fatal("failed: %v", err)
+		}
+
+		debug("%v", *res)
+
+		repos = append(repos, result...)
+
+		if res.NextPage == 0 {
+			break
+		}
+
+		opts.Page = res.NextPage
+	}
+
+	return repos
+}
+
+type PullRequestState struct {
+	Repository Repository
+	Pull *github.PullRequest
+	Reviews []*github.PullRequestReview
+}
+
+func describePull(ctx context.Context, client *github.Client, org string, repo string, num int) PullRequestState {
+	result, res, err := client.PullRequests.Get(ctx, org, repo, num)
+	if err != nil {
+		fatal("failed: %v", err)
+	}
+
+	debug("%v", *res)
+
+	debug("%d %d %d", result.GetDeletions(), result.GetAdditions(), result.GetMerged())
+
+	opts := &github.ListOptions{
+		PerPage: 30,
+	}
+	var reviews []*github.PullRequestReview
+	for {
+		result, res, err := client.PullRequests.ListReviews(ctx, org, repo, num, opts)
+		if err != nil {
+			fatal("failed: %v", err)
+		}
+
+		debug("%v", res)
+
+		reviews = append(reviews, result...)
+
+		if res.NextPage == 0 {
+			break
+		}
+
+		opts.Page = res.NextPage
+	}
+
+	return PullRequestState{
+		Repository: Repository(repo),
+		Pull: result,
+		Reviews: reviews,
+	}
+}
+
+func listPulls(ctx context.Context, client *github.Client, org string, repo string, st, et time.Time) []PullRequestState {
+	var pulls []PullRequestState
+
+	opts := &github.PullRequestListOptions{
+		State: "all",
+		ListOptions: github.ListOptions{PerPage: 30},
+	}
+
+	for {
+		result, res, err := client.PullRequests.List(ctx, org, repo, opts)
+		if err != nil {
+			fatal("failed: %v", err)
+		}
+
+		debug("%v", *res)
+
+		out := false
+		for _, pull := range result {
+			if pull.GetCreatedAt().Before(st) {
+				out = true
+				break
+			}
+			if pull.GetCreatedAt().After(et) {
+				// ignore
+				break
+			}
+
+			pulls = append(pulls, describePull(ctx, client, org, repo, pull.GetNumber()))
+		}
+		if out {
+			break
+		}
+
+		if res.NextPage == 0 {
+			break
+		}
+
+		opts.Page = res.NextPage
+	}
+
+	return pulls
+}
+
 func stats(_ *cobra.Command, _ []string) {
-	fmt.Println("todo")
+	rawUrl := viper.GetString(urlKey)
+	org := viper.GetString(orgKey)
+	start := viper.GetString(startKey)
+	end := viper.GetString(endKey)
+
+	layout := "2006-01-02"
+	st, err := time.Parse(layout, start)
+	if err != nil {
+		fatal("failed: %v", err)
+	}
+
+	et, err := time.Parse(layout, end)
+	if err != nil {
+		fatal("failed: %v", err)
+	}
+
+	if st.After(et) {
+		fatal("start %v >= end %v", st, et)
+	}
+
+	ctx := context.Background()
+
+	debug("%s", rawUrl)
+	client := github.NewClient(nil)
+
+	baseUrl, err := url.Parse(rawUrl)
+	if err != nil {
+		fatal("failed: %v", err)
+	}
+
+	client.BaseURL = baseUrl
+
+	debug("GET /orgs/%s/repos", org)
+
+	repos := listRepos(ctx, client, org)
+
+	contributors := &Contributors{}
+
+	for _, repo := range repos {
+		debug("GET /repos/%s/%s/pulls (from %s to %s)", org, *repo.Name, start, end)
+
+		pulls := listPulls(ctx, client, org, *repo.Name, st, et)
+
+		for _, pull := range pulls {
+			contributors.stats(pull)
+		}
+	}
+
+	data, err := json.Marshal(contributors)
+	if err != nil {
+		fatal("failed: %v", err)
+	}
+
+	fmt.Println(string(data))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/nokamoto/print-github-stats
 
 go 1.13
 
-require github.com/spf13/cobra v0.0.5
+require (
+	github.com/google/go-github/v28 v28.1.1
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
+)
